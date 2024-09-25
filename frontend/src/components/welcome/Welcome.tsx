bed4302315b8226c65d7cfcfbd50913f96c71b0b
yougotnothing <loool.ke.k.6.6.6@gmail.com>
2024-09-25 17:59:45 +0200

    Edit Welcome page.

--- frontend/src/components/welcome/Welcome.tsx
+++ frontend/src/components/welcome/Welcome.tsx
@@ -1,88 +1,87 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { EffectComposer } from 'three/examples/jsm//postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 
 export const Welcome = () => {
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const starColors = ['#fff', '#ebb2fd', '#ac6cd6', '#adfff1', '#9678e7'];
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       1,
-      5000
+      1000
     );
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-    camera.position.z = 200;
+    for (let i = 0; i < 2000; i++) {
+      const star = new THREE.Mesh(
+        new THREE.SphereGeometry(0.2, 32, 32, 32),
+        new THREE.MeshBasicMaterial({
+          color: '#fff',
+        })
+      );
+
+      star.position.set(0, 0, -350);
+
+      setTimeout(() => {
+        star.position.set(
+          (Math.random() - 0.5) * 350,
+          (Math.random() - 0.5) * 350,
+          (Math.random() - 0.5) * 350
+        );
+      }, 3000);
+
+      scene.add(star);
+    }
+
+    camera.position.z = 50;
 
     renderer.setClearColor(new THREE.Color('#000'));
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
     wrapperRef.current?.appendChild(renderer.domElement);
 
-    const starGeometry = new THREE.SphereGeometry(1, 32, 32);
-
-    const createStar = (color: string, position: THREE.Vector3) => {
-      const starMaterial = new THREE.MeshBasicMaterial({ color });
-      const star = new THREE.Mesh(starGeometry, starMaterial);
-      star.position.copy(position);
-      star.castShadow = true;
-      star.receiveShadow = true;
-
-      const glowMaterial = new THREE.MeshBasicMaterial({
-        color,
-        transparent: true,
-        opacity: 0.3,
-      });
-      const glow = new THREE.Mesh(starGeometry, glowMaterial);
-      glow.scale.set(2, 2, 2);
-      glow.position.copy(position);
-      glow.castShadow = false;
-      glow.receiveShadow = false;
-
-      scene.add(glow, star);
+    const mouse = { x: 0, y: 0, z: 0 };
+    const handleMouseMove = (e: MouseEvent) => {
+      mouse.x = (e.clientX / window.innerWidth) * 50 - 1;
+      mouse.y = -(e.clientY / window.innerHeight) * 50 + 1;
     };
 
-    for (let i = 0; i < 1500; i++) {
-      const color = starColors[Math.floor(Math.random() * starColors.length)];
-      const position = new THREE.Vector3(
-        (Math.random() - 0.5) * 2000,
-        (Math.random() - 0.5) * 2000,
-        (Math.random() - 0.5) * 2000
-      );
-      createStar(color, position);
-    }
+    const composer = new EffectComposer(renderer);
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
 
-    const light = new THREE.DirectionalLight('#fff', 1);
-    light.position.set(0, 0, 1).normalize();
-    scene.add(light);
+    const bloomPass = new UnrealBloomPass(
+      new THREE.Vector2(window.innerWidth, window.innerHeight),
+      1.7,
+      0.1,
+      0.2
+    );
+    composer.addPass(bloomPass);
 
-    const mouse = { x: 0, y: 0 };
-    const onMouseMove = (e: MouseEvent) => {
-      mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-      mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
-    };
-
-    window.addEventListener('mousemove', onMouseMove);
-
-    const animate = () => {
+    function animate() {
       requestAnimationFrame(animate);
 
-      scene.rotation.x = mouse.y * 0.1; // Rotation based on mouse movement
-      scene.rotation.y = mouse.x * 0.1;
+      scene.position.x = mouse.x * 0.1;
+      scene.position.y = mouse.y * 0.1;
 
-      renderer.render(scene, camera);
-    };
+      composer.render();
+    }
 
     animate();
 
+    window.addEventListener('mousemove', handleMouseMove);
+
     return () => {
       wrapperRef.current?.removeChild(renderer.domElement);
-      window.removeEventListener('mousemove', onMouseMove);
+      window.removeEventListener('mousemove', handleMouseMove);
     };
   }, []);
 
-  return <div ref={wrapperRef}></div>;
+  return (
+    <div ref={wrapperRef} style={{ width: '100vw', height: '100vh' }}></div>
+  );
 };
